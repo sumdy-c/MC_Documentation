e45f2bc90c06dcd01ffe72596b26643bd9f458b4
Sumdy <sumdy@mail.ru>
2024-01-12 11:33:44 +0300

    add new oage doc

--- components/doc/TestBlock.js
+++ components/doc/TestBlock.js
@@ -1,8 +1,24 @@
+class Example extends MC {
+    constructor(props) {
+        super();
+    }
+
+    render(states, props) {
+        console.log(states);
+        return $('<div>').html('Я компонент!');
+    }
+};
+
+const state = MC.createState('Привет');
+const state2 = MC.createState('Вуаля');
+
 const TestBlock = () => {
 
 
     return $('<div>').attr('id', 'test').addClass('doc_content_wrapper').append(
-        
-
+        $(Example, MC.Props({
+            props: 'Hi i props!',
+            states: [state, state2],
+        }), 'key'),
     )
 };
