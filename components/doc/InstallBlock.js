fe5c0f16cdc042f58fb2412b415765dfe011c2e6
Sumdy <sumdy@mail.ru>
2024-01-15 09:57:24 +0300

    add fix doc

--- components/doc/InstallBlock.js
+++ components/doc/InstallBlock.js
@@ -2,10 +2,12 @@ const InstallBlock = () => {
     return $('<div>').addClass('doc_content_wrapper').append(
         title('Установка'),
         infoBlock(
-            subTitle('Пока библиотека в режиме демонстрации, использование с сборщиками временно не доступно. Вы можете попробовать использование MC локально.')
+            subTitle('Пока библиотека в режиме демонстрации, использование с сборщиками ( пр. webpack, vite ) временно не доступно. Вы можете попробовать использование MC локально.')
         ),
 
-        $('<a>').addClass('link').attr('href', './lib/MC.min.js').text('Перейти к файлу'),
+        text('Вы можете подключить Micro Component локально'),
+
+        $('<a>').addClass('link').attr('href', './lib/MC.min.js').text('Посмотреть код'),
 
         text('Или установить с помощью: '),
         $('<a>').addClass('link').attr('href', 'https://www.npmjs.com/package/jquery-micro_component').text('NPM'),
@@ -14,8 +16,12 @@ const InstallBlock = () => {
             'npm i jquery-micro_component'
         ),
 
-        text('в index.html можно подключить с помощью:'), // &lt;div&gt;
-        codeBlock('&lt;script src="./node_modules/jquery-micro_component/MC.min.js"&gt;&lt;/script&gt;')
+        text('в index.html можно подключить с помощью:'),
+        codeBlock('&lt;script src="./node_modules/jquery-micro_component/MC.min.js"&gt;&lt;/script&gt;'),
+
+        alertBlock(
+            text('Обратите внимание, что Micro Component должен быть подключен после jQuery.')
+        )
 
     );
 }
